2721b3f2e075eebac14977bf87c1d537890dc3ad
Alexandre CERF <alexandrecerf.pro@gmail.com>
2026-09-14 19:21:43 +0200

    feat(cart): point d entree d effacement RGPD

--- templates/back/services/cart-service/src/repositories/cart.repository.ts
+++ templates/back/services/cart-service/src/repositories/cart.repository.ts
@@ -3,4 +3,5 @@ import { Cart } from '../entities/cart.entity';
 export abstract class ICartRepository {
   abstract findBySessionKey(sessionKey: string): Promise<Cart | null>;
   abstract save(cart: Cart): Promise<Cart>;
+  abstract deleteByCustomerId(customerId: string): Promise<number>;
 }
